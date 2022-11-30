20481e9224b5384b9f2113003781af1d19747e10
Thomas Da Silva <dasilva.thomas87@gmail.com>
2022-11-30 09:35:53 +0100

    Ajout Orders (crud)

--- src/main/java/com/example/tpFilRouge/controller/OrdersController.java
+++ src/main/java/com/example/tpFilRouge/controller/OrdersController.java
@@ -1,10 +1,63 @@
 package com.example.tpFilRouge.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.tpFilRouge.model.Orders;
+import com.example.tpFilRouge.service.OrdersDirectory;
 
 @RestController
 @RequestMapping("api")
 public class OrdersController {
-
+	@Autowired
+	private OrdersDirectory ordersDirectory;
+	
+	@GetMapping("orders")
+	public List<Orders> getOrders() {
+		return ordersDirectory.getOrders();
+	}
+	
+	@GetMapping("order/{id}")
+	public ResponseEntity<Orders> getOrders(@PathVariable("id") Long id){
+		Optional<Orders> order = ordersDirectory.getOrder(id);
+		if(order.isEmpty()){
+			return ResponseEntity.notFound().build();
+		}
+		else{
+			return ResponseEntity.ok(order.get());
+		}
+	}
+	
+	@PostMapping("order")
+	public Orders postOrder(@RequestBody Orders newOrder) {
+		ordersDirectory.postOrder(newOrder);		
+		return newOrder;
+	}
+	
+	@DeleteMapping("order/{id}")
+	public void deleteOrder(@PathVariable("id") Long id){
+		ordersDirectory.deleteOrder(id);
+	}
+	
+	@PutMapping("order/{id}")
+	public ResponseEntity<Orders> updateOrder(@RequestBody Orders order, @PathVariable("id") Long id){
+		if(!order.getId().equals(id)){
+			return ResponseEntity.badRequest().build();
+		}
+		else {
+			ordersDirectory.updateOrder(order, id);
+			return ResponseEntity.ok().build();
+		}
+	}
 }
--- src/main/java/com/example/tpFilRouge/service/OrdersDirectory.java
+++ src/main/java/com/example/tpFilRouge/service/OrdersDirectory.java
@@ -1,5 +1,36 @@
 package com.example.tpFilRouge.service;
 
-public class OrdersDirectory {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.tpFilRouge.dao.OrdersRepository;
+import com.example.tpFilRouge.model.Orders;
+
+@Service
+public class OrdersDirectory {
+	@Autowired
+	private OrdersRepository ordersRepository;
+
+	public List<Orders> getOrders() {
+		return ordersRepository.findAll();
+	}
+	
+	public Optional<Orders> getOrder(Long id) {
+		return ordersRepository.findById(id);
+	}
+	
+	public void postOrder(Orders newOrder) {
+		ordersRepository.save(newOrder);
+	}
+	
+	public void deleteOrder(Long id){
+		ordersRepository.deleteById(id);
+	}
+
+	public void updateOrder(Orders orderToUpdate, Long id){
+		ordersRepository.save(orderToUpdate);
+	}
 }
